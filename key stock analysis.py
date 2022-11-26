c273df345f2c741ab156bc44a40169a606b27bf2
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2022-11-25 21:57:43 -0600

    Update key stock analysis.py
    Amended some functions but the meat of the update was adding the finder function that I was using to scrape the data from the front page of a given ticker on yahoo finance. It was fundamentally a exercise in basic regex as well as iterating through data and lists to create dictionaries (conversion to JSON). 
    
    I did find the use case to be extremely limited especially considering the amount of code it is, this was not by design, after some experimenting with selenium web driver i found the .text() function cleaned everything up much easier than I was able to. I'm still choosing to include it because I think it helps break down some fundamentals of regex, .find(), and splicing.

--- key stock analysis.py
+++ key stock analysis.py
@@ -42,3 +42,51 @@ def d2e(debt, shareholder_equity):
         return print(f"Debt to Equity = {x} \n Higher than average debt, consider if industry has high fixed assets")
 
 
+def finder(container):
+    import re
+    str(container)
+    # Define the empty lists and dictionary for use
+    x = 0
+    y = 0
+    value_list = []
+    title_list = []
+    title_val = {}
+
+    # While loop based off of 'title' variable so iterates until it gets to final title. Values always come after title.
+    last_instance = container.rfind("title=")
+    while x < last_instance:
+        search_point = container[x:]
+        # Find the first title and then isolate the actual 'title' using splice and regex
+        title_loc = search_point.find("title=")
+        title = search_point[title_loc:(title_loc + 75)]
+        # Regex function is simple but the specificity in the splice allows it to be so, thus easier to read
+        title = (re.findall(r'"(.*?)"', title))
+        # The only failure in the simple regex is getting 'Va(m)' put into the list.
+        if 'Va(m)' in title:
+            title.pop()
+        # Depending on the length of the title itself sometimes the regex will grab it again due to Yahoo making it a
+        # button on the finance statement. EXPERIMENT WITH .SEARCH() FUNCTION!!
+        if len(title) > 1:
+            title.pop()
+        title_list.extend(title)
+
+        # Repeat the process to get the value that comes after the title. Need to find a way to get ALL variables
+        # following the title (the year after year). Expanding the search_point would be a place to start.
+        val_loc = search_point.find('data-test="fin-col"')
+        val = search_point[val_loc:(val_loc + 80)]
+        val = re.findall(r"<span>(.*?)</span>", val)
+        # If the value is empty there will literally be nothing in the html so needs a placeholder for dict to work
+        # correctly
+        if len(val) < 1:
+            val.extend('-')
+        value_list.extend(val)
+        # 1,000 here isn't arbitrary, it is approx the minimum to get past the "fin-col" values
+        x = x + (title_loc + 1000)
+
+    # Creating the dict would be fewer lines with a pop function but this method keeps the lists intact.
+    # Value list here is arbitrary.
+    for i in value_list:
+        title_val[title_list[y]] = value_list[y]
+        y += 1
+    return title_val
+
